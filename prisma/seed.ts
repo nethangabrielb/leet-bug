eda5d0d0bf36941f359127fc8f15fcdb6f24a306
Nethan B. <bagasbas.nethangabriel@gmail.com>
2026-04-18 23:27:04 +0800

    Initial commit

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+import { Prisma, PrismaClient } from "../orm/generated/prisma/client";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: "Alice",
+    email: "alice@prisma.io",
+  },
+  {
+    name: "Bob",
+    email: "bob@prisma.io",
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
